705b6670b83e38a553589346b143ffa93156361e
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-02 14:52:48 +0200

    [UPD] ciclo for

--- src/components/ListaCard.vue
+++ src/components/ListaCard.vue
@@ -1,133 +1,28 @@
 <script >
 export default {
     name: 'ListaCard',
+    props: ['img', 'backImage', 'marca', 'nome', 'prezzo']
 
 }
 </script>
 
 <template>
-    <div class="riga">
-        <div class="col-33">
-            <div class="contenuto">
-                <div class="img-shop">
-                    <img class="img-shop main-img" src="/public/img/1.webp" alt="foto">
-                    <img class="img-shop hover-img" src="/public/img/1b.webp" alt="foto-h">
-                    <div class="labels">
-                        <div class="label red">-50 %</div>
-                        <div class="label green">Sostenibilità</div>
-                    </div>
-                    <div class="cuore">
-                        &hearts;
-                    </div>
-                </div>
-                <div>
-                    <span class="marchio">Levi's</span>
-                    <p><strong>RELAXED FIT TEE UNISEX</strong></p>
-                    <span class="prezzo"><strong>14,99 €</strong></span>
-                    <span class="prezzo-vecchio">29,99 €</span>
-                </div>
+    <div class="contenuto col-33">
+        <div class="img-shop">
+            <img class="img-shop main-img" :src="img" :alt="foto">
+            <img class="img-shop hover-img" :src="backImage" :alt="foto">
+            <div class="labels">
+                <div class="label red">-50 %</div>
+                <div class="label green">Sostenibilità</div>
+            </div>
+            <div class="cuore">
+                &hearts;
             </div>
         </div>
-
-        <div class="col-33">
-            <div class="contenuto">
-                <div class="img-shop">
-                    <img class="img-shop main-img" src="/public/img/2.webp" alt="foto">
-                    <img class="img-shop hover-img" src="/public/img/2b.webp" alt="foto-h">
-                    <div class="labels">
-                        <div class="label red">-50 %</div>
-                    </div>
-                    <div class="cuore">
-                        &hearts;
-                    </div>
-                </div>
-                <div>
-                    <span class="marchio">Levi's</span>
-                    <p><strong>RELAXED FIT TEE UNISEX</strong></p>
-                    <span class="prezzo"><strong>14,99 €</strong></span>
-                    <span class="prezzo-vecchio">29,99 €</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-33">
-            <div class="contenuto">
-                <div class="img-shop">
-                    <img class="img-shop main-img" src="/public/img/3.webp" alt="foto">
-                    <img class="img-shop hover-img" src="/public/img/3b.webp" alt="foto-h">
-                    <div class="labels">
-                        <div class="label red">-50 %</div>
-                    </div>
-                    <div class="cuore">
-                        &hearts;
-                    </div>
-                </div>
-                <div>
-                    <span class="marchio">Levi's</span>
-                    <p><strong>RELAXED FIT TEE UNISEX</strong></p>
-                    <span class="prezzo"><strong>14,99 €</strong></span>
-                    <span class="prezzo-vecchio">29,99 €</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-33">
-            <div class="contenuto">
-                <div class="img-shop">
-                    <img class="img-shop main-img" src="/public/img/4.webp" alt="foto">
-                    <img class="img-shop hover-img" src="/public/img/4b.webp" alt="foto-h">
-                    <div class="labels">
-                        <div class="label red">-50 %</div>
-                        <div class="label green">Sostenibilità</div>
-                    </div>
-                    <div class="cuore">
-                        &hearts;
-                    </div>
-                </div>
-                <div>
-                    <span class="marchio">Levi's</span>
-                    <p><strong>RELAXED FIT TEE UNISEX</strong></p>
-                    <span class="prezzo"><strong>14,99 €</strong></span>
-                    <span class="prezzo-vecchio">29,99 €</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-33">
-            <div class="contenuto">
-                <div class="img-shop">
-                    <img class="img-shop main-img" src="/public/img/5.webp" alt="foto">
-                    <img class="img-shop hover-img" src="/public/img/5b.webp" alt="foto-h">
-                    <div class="cuore">
-                        &hearts;
-                    </div>
-                </div>
-                <div>
-                    <span class="marchio">Levi's</span>
-                    <p><strong>RELAXED FIT TEE UNISEX</strong></p>
-                    <span class="prezzo"><strong>14,99 €</strong></span>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-33">
-            <div class="contenuto">
-                <div class="img-shop">
-                    <img class="img-shop main-img" src="/public/img/6.webp" alt="foto">
-                    <img class="img-shop hover-img" src="/public/img/6b.webp" alt="foto-h">
-                    <div class="labels">
-                        <div class="label green">Sostenibilità</div>
-                    </div>
-                    <div class="cuore">
-                        &hearts;
-                    </div>
-                </div>
-                <div>
-                    <span class="marchio">Levi's</span>
-                    <p><strong>RELAXED FIT TEE UNISEX</strong></p>
-                    <span class="prezzo"><strong>14,99 €</strong></span>
-                </div>
-            </div>
+        <div>
+            <span class="marchio">{{ marca }}</span>
+            <p><strong>{{ nome }}</strong></p>
+            <span class="prezzo"><strong>{{ prezzo }}</strong></span>
         </div>
     </div>
 </template>
